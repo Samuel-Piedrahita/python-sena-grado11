6de41b604f4271225dfb3904903fcd0074ade5ec
Samuel-Piedrahita <programacionpiedrahita@gmail.com>
2026-07-09 16:17:21 -0500

    Add files via upload

---
+++ presentacion.py
@@ -0,0 +1,15 @@
+#Mi primera contribución a GitHub
+
+nombre= "Samuel Piedrahita"
+grado="11°"
+colegio="SENA"
+
+lenguaje = "Python 🐍"
+print("=" * 45)
+print(f" Hola GitHub! Soy {Samuel}")
+print(f" Grado: {11} | Institución: {Francisco Jose De Caldas}")
+print(f" Aprendiendo: {Python}")
+print(f" ¡Este es mi primer commit! ")
+print("=" * 45)
+
+
